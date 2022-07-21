32ab644478cbced9a0e342d8b0836e94356d856e
wilbur61 <wroales@perscholas.org>
2022-07-20 22:01:26 -0400

    Added my project

---
+++ src/main/java/com/example/AC5demo/Ac5demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AC5demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac5demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac5demoApplication.class, args);
+	}
+
+}
